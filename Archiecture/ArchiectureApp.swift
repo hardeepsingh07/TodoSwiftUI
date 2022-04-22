7484bb20615d41de84eea41cda3db8df3f50b72c
Hardeep Singh <hardeep07@yahoo.com>
2022-04-21 22:15:29 -0700

    Initial Commit

---
+++ Archiecture/ArchiectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiectureApp.swift
+//  Archiecture
+//
+//  Created by Hardeep Singh on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ArchiectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
